30762c8d71fcba8915558ca798fd1f8758f3ac18
Diego <pasayealvarado@gmail.com>
2025-10-17 14:42:39 -0600

    cambios

---
+++ src/app/inicio/components/general-info/general-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-info',
+  imports: [],
+  templateUrl: './general-info.html',
+  styleUrl: './general-info.css'
+})
+export class GeneralInfo {
+
+}
