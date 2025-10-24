09c93e8c645d02715f0570a867726f46f94e041e
Diego <pasayealvarado@gmail.com>
2025-10-23 20:20:31 -0600

    estructura frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./inicio/inicio-module').then(m => m.InicioModule)
+    }
+];
--- src/app/inicio/inicio-page/inicio-page.ts
+++ src/app/inicio/inicio-page/inicio-page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Header } from '../components/header/header';
+import { Leaderboard } from '../components/leaderboard/leaderboard';
+import { Requirements } from '../components/requirements/requirements';
+import { GeneralInfo } from '../components/general-info/general-info';
+import { Footer } from '../components/footer/footer';
+import { NavBar } from '../components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-inicio-page',
-  imports: [],
+  imports: [Header, Leaderboard, Requirements, GeneralInfo, Footer, NavBar],
   templateUrl: './inicio-page.html',
   styleUrl: './inicio-page.css'
 })
