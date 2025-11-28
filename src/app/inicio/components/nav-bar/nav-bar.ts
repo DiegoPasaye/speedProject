d27eb223cf534d83f2e390ce0b5b154bf9b275e9
Diego <pasayealvarado@gmail.com>
2025-11-27 20:29:46 -0600

    soluciones en inicio

--- src/app/inicio/components/nav-bar/nav-bar.ts
+++ src/app/inicio/components/nav-bar/nav-bar.ts
@@ -14,6 +14,7 @@ import { AuthService } from '../../../services/auth.service';
   providers: [provideIcons({ heroUserCircle })]
 })
 export class NavBar {
+  isMenuOpen = false;
 
   constructor(public authService: AuthService) {}
 
@@ -23,5 +24,13 @@ export class NavBar {
 
   get userName(): string {
     return this.authService.currentUser?.profile?.display_name || 'Usuario';
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
