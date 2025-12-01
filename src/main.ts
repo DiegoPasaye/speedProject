1c3c8b2134404af646053e1579a17b1b913a4e70
Diego <pasayealvarado@gmail.com>
2025-11-30 20:43:47 -0600

    cambios

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
